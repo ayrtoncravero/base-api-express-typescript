9c0af122a56ebe2a352ca27599ba553286f1901f
ayrton cravero <ayrtoncravero26@gmail.com>
2024-09-07 09:51:58 -0300

    add dotenv :eyes:

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
